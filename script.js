c2ff88e48ae77b392279a40a297cc34b40b964c0
linama5 <lm.acosta545@gmail.com>
2021-10-14 13:32:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,21 @@ console.log("script.js is loaded");
 const toDoInput = document.querySelector(".taskInput");
 const toDoButton = document.querySelector(".addButton");
 const toDoList = document.querySelector(".todoList");
-//const alertdiv = document.querySelector("alert")
 const alertp = document.querySelector(".alert");
+
+const number = document.createElement("p");
+const numberdiv = document.querySelector(".numberOfComplete");
+
+const todoListArray = [];
 
 //event listeners
 
 toDoButton.addEventListener("click", addingTask);
 //toDoList.addEventListener("click", eventClick);
+
+toDoList.addEventListener("click", eventClick);
+
+toDoInput.addEventListener("click", countToCero);
 
 //functions
 
@@ -21,7 +29,6 @@ function addingTask(event) {
 
   //verifying that the text field is no empty
   if (!toDoInput.value) {
-    
     alertp.innerHTML = "input must not be empty";
 
     return false;
@@ -51,5 +58,58 @@ function addingTask(event) {
 
     //append div to the list we have in HTML
     toDoList.appendChild(todoDiv);
+
+    //push object to array
+    todoListArray.push(todoDiv);
+
+    //clear the textfile
+    toDoInput.value = "";
   }
+}
+
+function eventClick(e) {
+  //find which button is clicked
+
+  const buttonOnclicked = e.target;
+
+  if (buttonOnclicked.classList[0] === "deleteButton") {
+    //grab the parent of the element we want to delete
+    const parent = buttonOnclicked.parentElement;
+    parent.remove();
+
+    children = parent.children[0];
+    // console.log(children);
+
+    for (let i = 0; i < todoListArray.length; i++) {
+      if (parent == todoListArray[i]) {
+        //remove one div from the array in that Index
+        todoListArray.splice([i], 1);
+      }
+    }
+  } else {
+    const parent1 = buttonOnclicked.parentElement;
+    parent1.classList.toggle("completed");
+
+   myCounter();
+    }
+  }
+
+
+function myCounter() {
+  var count = 0;
+  for (let i = 0; i < todoListArray.length; i++) {
+    if (todoListArray[i].classList == "todo completed") {
+      ++count;
+
+      number.innerText = count;
+      numberdiv.appendChild(number);
+    }
+}
+}
+
+function countToCero(){
+
+number.innerHTML = 0;
+numberdiv.append(number);
+
 }
